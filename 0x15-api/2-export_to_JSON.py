df3aab122364795158dd263b52320760d14724d3
brk-a <fnjakai@outlook.com>
2022-01-31 04:22:44 -0800

    Tasks 2 & 3: try v 1.0

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Gather todo data from an API and write to JSON file
+"""
+
+import json
+import requests
+import sys
+
+
+def get_username(base_url, user_id):
+    """Gets username"""
+    response = requests.get(
+        "{}users/{}".format(base_url, user_id))
+    usr_dict = response.json()
+    return usr_dict['username']
+
+
+def get_todo_list(base_url, user_id):
+    """Gets todo list"""
+    response = requests.get(
+        "{}users/{}/todos".format(base_url, user_id))
+    return response.json()
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    uname = get_username(base_url, user_id)
+    todo_list = get_todo_list(base_url, user_id)
+
+    with open('{}.json'.format(user_id), 'w') as f:
+        todo_dict = {}
+        todo_dict[sys.argv[1]] = []
+        for todo in todo_list:
+            task = {}
+            task['task'] = todo['title']
+            task['completed'] = todo['completed']
+            task['username'] = uname
+            todo_dict[sys.argv[1]].append(task)
+
+        f.write(json.dumps(todo_dict))
